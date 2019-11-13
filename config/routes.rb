ad7b58c156bccd377ed8a25f12297bc4493ed623
suguru6213 <t6v2x1q3@gmail.com>
2019-11-13 19:13:29 +0900

    update_haml_scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "personal_registrations#profile_edit"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "personal_registrations#credit_registration"
+
+# credit_registration
+# profile_edit
+# personal
+# logout
+
 end
