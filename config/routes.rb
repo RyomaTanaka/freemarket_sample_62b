4ca962aaf529ba1c1e5ce76d45cc29bda73c88b4
koheiymgw <pgsk0698@icloud.com>
2019-11-14 18:33:55 +0900

    item_detail_page-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'user/index'
   devise_for :users
-  root 'item#index'
+  root 'items#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
