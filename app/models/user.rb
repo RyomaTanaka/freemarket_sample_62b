7f30a5569c4f47eb0ca105138e488c88a4efd8e6
mimisenn <53231640+mimisenn@users.noreply.github.com>
2019-11-15 13:55:54 +0900

    profile　モデルチェンジ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   
   has_many :addresses, dependent: :destroy
-  accepts_nested_attributes_for :addresses
+  has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :addresses, :profile
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_032858) do
+ActiveRecord::Schema.define(version: 2019_11_15_044024) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -34,8 +34,17 @@ ActiveRecord::Schema.define(version: 2019_11_15_032858) do
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "nickname", default: "", null: false
+    t.text "introduction"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -54,4 +63,5 @@ ActiveRecord::Schema.define(version: 2019_11_15_032858) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
+  add_foreign_key "profiles", "users"
 end
