660d7bc424b5b7c6bc324f8626b1ff0aba56e231
suguru6213 <t6v2x1q3@gmail.com>
2019-11-15 13:53:24 +0900

    update_haml_scss

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,5 +2,16 @@ class MypagesController < ApplicationController
   def index
   end
 
+  def logout
+  end
+
+  def credit_registration
+  end
+
+  def personal
+  end
+
+  def profile_edit
+  end
 
 end
