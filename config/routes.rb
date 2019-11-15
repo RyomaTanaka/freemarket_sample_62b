616ec2be29240a9296342850a7c85a977bfab7c1
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-11-15 20:32:03 +0900

    new_sell_1

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'user/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "markets#index"
-  resources :markets
-  resources :sells
+  root to: "items#sell"
+  resources :items
 
 end
