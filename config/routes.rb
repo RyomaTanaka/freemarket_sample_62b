c9d69baea0a65230ed063a4f55833ed3f697d2eb
suguru6213 <t6v2x1q3@gmail.com>
2019-11-18 15:30:42 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
 
-
+  get 'purchase' => 'items#purchase'
+  get 'profile_edit' => 'mypages#profile_edit'
+  get 'personal' => 'mypages#personal'
+  get 'logout' => 'mypages#logout'
+  get 'mypage' => 'mypages#index'
+  get 'mypages/credit_registration'
   get 'user/index'
+  get 'item-detail' => 'items#show'
+  get 'items-new' => 'items#new'
+  
+ 
+  get 'address' => 'mypages#address'
+  get 'card' => 'mypages#card'
+  get 'complete' => 'mypages#complete'
+  get 'edit' => 'mypages#edit'
+  get 'new' => 'mypages#new'
+  get 'tele' => 'mypages#tele'
+
+
+
+
+
+
   devise_for :users
   root "items#index"
   resources :items
