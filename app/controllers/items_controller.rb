6b11f555a8b437c7b35ea5a9e94b16d6f17c4303
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-11-18 11:05:25 +0900

    Exhibit_1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
+
   def show
   end
   def index
+    # トップページ
+    @items = Item.all
   end
+
   def new
+    # 商品出品
+    @item =  Item.new(item_params)
+  end
+
+  def create
+    # 出品内容を保存
+    Item.create(item_params)
+    binding.pry
+    # @item.user_id = current_user.id
+    # @item.save
+
+  end
+
+  private
+  def item_params
+    params.permit(:name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :addresses, :profile
 
+  # ----------------------------------------
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :validatable, password_length: 7..128
