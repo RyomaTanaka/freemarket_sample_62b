e1f94067e60dadbc869c73d5ac35ab6c113c776d
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-11-19 17:15:55 +0900

    Exhibit_3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,15 @@ class ItemsController < ApplicationController
   def index
     # トップページ
     @shipping = Shipping.all
+    
   end
 
   def new
     # 商品出品
     @shipping = Shipping.new
-    @shipping.items.build
+    item = @shipping.items.build
+    item.images.build
+    # binding.pry
 
     # @item.images.build
 
@@ -18,17 +21,14 @@ class ItemsController < ApplicationController
 
   def create
     # 出品内容を保存
-    @shipping = Shipping.new(shipping_params)
-    @shipping.save
+    @shipping = Shipping.create(shipping_params)
 
-    redirect_to :root
 
   end
 
   private
   def shipping_params
     params.require(:shipping).permit(:cost_burden, :period_before_shipping, :prefecure,
-    items_attributes: [:name, :body, :price, :condition])
-
+    items_attributes: [:name, :body, :status, :price, :condition, images_attributes: [:image_url]])
   end
 end
