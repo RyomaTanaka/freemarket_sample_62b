974afc01034837546824c25083ad73d2f57ae3bc
mimisenn <53231640+mimisenn@users.noreply.github.com>
2019-11-21 18:47:46 +0900

    as

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2019_11_16_045107) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id", null: false
-    t.string "image_url", null: false
+    t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_images_on_item_id"
