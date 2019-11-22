d612bb89afeefbf8fa5efa2fa769b4c29bc22a92
RyomaTanaka <tanakaryoma1992@gmail.com>
2019-11-22 15:44:10 +0900

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users,
   controllers: {
     sessions: 'users/sessions',
@@ -7,24 +6,6 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   
-  root "items#index"
-  resources :items do
-    collection do
-      # get 'mypages/:id' => 'mypages#index'
-      get "show" => "items#show"
-      get "exhibited_lists/:id" => "items#exhibited_lists"
-      get "exhibited/:id" => "items#exhibited"
-      delete 'items/:id' => 'items#destroy'
-      get "/items/:id/edit" => "items#edit"
-      get  'purchase/:id'=>  'items#purchase', as: 'purchase'
-
-      # post "show" => 
-      # post "delete"
-      # ... "edit"
-      # ... "update"
-    end
-  end
-
   resource :signups do
     get :step0
     get :step1
@@ -32,28 +13,24 @@ Rails.application.routes.draw do
     get :step3
     get :step4
   end
+  
+  resources :mypages, only:[:show] do
+    collection do
+      get :list_items
+      get :list_items_progress
+      get :list_items_completed
+      get :purchase
+      get :purchased
+      get :profile
+      get :card
+      get :card_create
+      get :personal
+      get :logout
+    end
+  end
+  
+  root "items#index"
+  resources :users
+  resources :items
   resources :cards
-  
-  get 'profile_edit' => 'mypages#profile_edit'
-  get 'personal' => 'mypages#personal'
-  get 'logout' => 'mypages#logout'
-  get 'mypage' => 'mypages#index'
-  get 'mypages/credit_registration'
-  get 'user/index'
-  get 'item-detail' => 'items#show'
-  get 'items-new' => 'items#new'
-  
-  
-  get 'address' => 'mypages#address'
-  get 'card' => 'mypages#card'
-  get 'complete' => 'mypages#complete'
-  get 'edit' => 'mypages#edit'
-  get 'new' => 'mypages#new'
-  get 'tele' => 'mypages#tele'
-
-  # get 'items/:id/edit'  => 'mypages#item_edit'
-
-
-  get 'users/:id'   =>  'users#show'
-
 end
