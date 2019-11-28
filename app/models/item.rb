36542d61f7537b4f581024cde92b1948172bc715
mimisenn <53231640+mimisenn@users.noreply.github.com>
2019-11-28 21:20:44 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
   
   validates :name, presence: true
