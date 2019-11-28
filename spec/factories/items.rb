3a04b938f694683fd9d32e59fe42699efcb77f63
mimisenn <53231640+mimisenn@users.noreply.github.com>
2019-11-28 20:38:35 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     price                  {"40000"}
     status                 {1}
     order_status           {"1"}
-    prefecture_id          {5}   
+    prefecture_id          {5}
     cost_burden            {"1"}
     period_before_shipping {"1"}
     created_at             { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
