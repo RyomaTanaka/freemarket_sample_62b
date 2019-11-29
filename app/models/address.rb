33ad93d97ba8f15804333e16f902ad4bd78b3f44
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-11-29 20:16:57 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :user
   
-  validates :user_name_ruby, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :prefecture, presence: true, format: {with: /\A[0-9-]{,14}\z/}
-  validates :city, presence: true, format: { with: /\A[一-龥ぁ-ん]/}
-  validates :house_number, presence: true, format: { with: /\A[一-龥ぁ-ん]/}
-  validates :post_number, presence: true, format: {with: /\A[0-9-]{,14}\z/}
+  validates :user_name_ruby, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
+  validates :post_number, presence: true
 end
