e617746344aa98889a1fc1905569f8d34e6758b7
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-11-29 12:21:01 +0900

    new_sell_3_commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,6 @@ class ItemsController < ApplicationController
 
   def index
 
-    # トップページ
     @items = Item.includes(:images).limit(10).order("created_at DESC")
     @images = Image.all
 
@@ -18,21 +17,14 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # 商品出品
     @item = Item.new
     image = @item.images.build
 
-    #商品カテゴリー
     @category_parent_array = ["---"]
-    # Categorie.where(ancestry: nil).each do |parent| 実装途中のためコメントアウト残してます
-    #   @category_parent_array << parent.name
-    # end
   end
 
   def create
 
-    #商品出品
-    # binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to action: :index
@@ -54,14 +46,6 @@ class ItemsController < ApplicationController
 
     redirect_to list_items_mypage_path, notice: '編集しました'
   end
-
-
-    # if @item.update(item_params)
-    #   redirect_to root_path
-    # else
-    #   redirect_to edit_item_path
-    # end
-
 
   def destroy
 
@@ -93,23 +77,16 @@ class ItemsController < ApplicationController
   private
 
   def set_item
-    #itemのidを持ってくる
+
     @item = Item.includes(:images).find(params[:id])
   end
 
   def item_params
-    #出品itemのparams
+
     params.require(:item).permit(:cost_burden, :period_before_shipping, :prefecture_id, :name, :body, :status, :order_status, :price, :shipping_method,
     images_attributes: [:image]).merge(user_id: current_user.id)
 
   end
-
-
-  # def item_params
-  #   #出品itemのparams
-  #   params.require(:item).permit(:cost_burden, :period_before_shipping, :prefecture, :name, :body, :status, :order_status, :price,
-  #   images_attributes: [:image]).merge(user_id: current_user.id)
-  # end
 
   def exihibited_lists
     @items = Item.where(user_id: current_user)
@@ -117,7 +94,7 @@ class ItemsController < ApplicationController
   end
 
   def exihibited
-    # @items = Item.where(user_id: current_user)
+
     @item = Item.find(params[:id])
 
   end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -17,16 +17,16 @@ FactoryBot.define do
   end
 end
 
-  factory :item do
-    name                   {"sample"}
-    body                   {"00000000"}
-    price                  {"40000"}
-    status                 {1}
-    order_status           {"1"}
-    prefecture_id          {5}
-    cost_burden            {"1"}
-    period_before_shipping {"1"}
-    created_at             { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
-    user
-  end
-end
+#   factory :item do
+#     name                   {"sample"}
+#     body                   {"00000000"}
+#     price                  {"40000"}
+#     status                 {1}
+#     order_status           {"1"}
+#     prefecture_id          {5}
+#     cost_burden            {"1"}
+#     period_before_shipping {"1"}
+#     created_at             { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+#     user
+#   end
+# end
