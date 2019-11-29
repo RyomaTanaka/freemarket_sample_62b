bd30b9a5ad057d89276460c9697855d6c990012d
mimisenn <53231640+mimisenn@users.noreply.github.com>
2019-11-29 17:30:14 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, class: User do
     nickname                    {"abe"}
     sequence(:email)            {Faker::Internet.email}
     password                    {"00000000"}
@@ -9,6 +9,19 @@ FactoryBot.define do
     family_name_ruby            {"タロウ"}
     birth_year                  {"2000"}
     birth_month                 {"10"}
+    birth_day                   {"2"}
+  end
+
+  factory :another_user, class: User do 
+    nickname                    {"kaneko"}
+    sequence(:email)            {Faker::Internet.email}
+    password                    {"01200417"}
+    first_name                  {"カネコ"}
+    family_name                 {"スグル"}
+    first_name_ruby             {"カネコ"}
+    family_name_ruby            {"スグル"}
+    birth_year                  {"1995"}
+    birth_month                 {"11"}
     birth_day                   {"2"}
   end
 end
