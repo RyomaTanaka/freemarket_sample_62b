cf4a2352db297a5293838b7a347f54a061c4672b
mimisenn <53231640+mimisenn@users.noreply.github.com>
2019-11-29 13:15:50 +0900

    item_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,11 +33,10 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(update_item_params)
-      redirect_to root_path
-    else
-      redirect_to edit_item_path
-    end
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    
+    redirect_to list_items_mypage_path, notice: '編集しました'
   end
 
   def destroy
