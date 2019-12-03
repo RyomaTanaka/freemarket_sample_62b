da5e47c12da234afe483a4d8f95158b552580140
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-12-03 13:40:32 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,20 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.limit(10).order("created_at DESC")
+    @categories = Categorie.where(params[:id])
+    @category_children1 = Categorie.where(ancestry: 1)
+    @category_children2 = Categorie.where(ancestry: 200)
+    @category_children3 = Categorie.where(ancestry: 346)
+    @category_children4 = Categorie.where(ancestry: 481)
+    @category_children5 = Categorie.where(ancestry: 625)
+    @category_children6 = Categorie.where(ancestry: 685)
+    @category_children7 = Categorie.where(ancestry: 798)
+    @category_children8 = Categorie.where(ancestry: 898)
+    @category_children9 = Categorie.where(ancestry: 984)
+    @category_children10 = Categorie.where(ancestry: 1093)
+    @category_children11 = Categorie.where(ancestry: 1147)
+    @category_children12 = Categorie.where(ancestry: 1207)
+    @category_children13 = Categorie.where(ancestry: 1270)
   end
     
   def show
