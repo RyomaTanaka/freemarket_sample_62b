0dd98f285aed47cede91b5fc1f5dd217463e80c3
RyomaTanaka <tanakaryoma1992@gmail.com>
2019-12-05 13:19:18 +0900

    addcolunm

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_12_05_025442) do
-
+ActiveRecord::Schema.define(version: 2019_12_05_041400) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
