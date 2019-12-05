2a06e89b725fb480a4d849be22110e7c2cfc6590
RyomaTanaka <tanakaryoma1992@gmail.com>
2019-12-05 12:27:09 +0900

    create_categoriy

--- db/migrate/20191116045107_create_categories.rb
+++ db/migrate/20191116045107_create_categories.rb
@@ -3,7 +3,10 @@ class CreateCategories < ActiveRecord::Migration[5.2]
     create_table :categories do |t|
       t.string :name, unique: true
       t.bigint :parent_id, foreign_key: true
+      t.string :ancestry
       t.timestamps
     end
+    
+    add_index :categories, :ancestry
   end
 end
